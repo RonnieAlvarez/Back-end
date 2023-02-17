1cb1a16cfc2ad8fa7ea28a59f7cb4a46478a859d
RonnieAlvarez <ronnie.alvarezc@gmail.com>
2023-02-17 12:02:47 -0600

    Ronnie_Alvarez_Entregable01

---
+++ Entregable01/RonnieAlvarez_Entregable01.js
@@ -0,0 +1,63 @@
+// Realizar una clase “ProductManager” que gestione un conjunto de productos.
+class productManager {
+  constructor() {
+    // Debe crearse desde su constructor con el elemento products, el cual será un arreglo vacío.
+    this.products = [];
+  }
+  // Cada producto que gestione debe contar con las propiedades:
+  // title (nombre del producto)
+  // description (descripción del producto)
+  // price (precio)
+  // thumbnail (ruta de imagen)
+  // code (código identificador)
+  // stock (número de piezas disponibles)
+
+  addProduct(title, description, price, thumbnail, code, stock) {
+    // Debe contar con un método “addProduct” el cual agregará un producto al arreglo de productos inicial.
+    let productObj = {
+      title: title,
+      description: description,
+      price: price,
+      thumbnail: thumbnail,
+      code: code,
+      stock: stock,
+      id: this.products.length + 1,
+      // Al agregarlo, debe crearse con un id autoincrementable
+    };
+    // Validar que no se repita el campo “code” y que todos los campos sean obligatorios
+    const requerido = Object.values(productObj).includes(null || undefined);
+    if (requerido) {
+      console.log("Todos los campos son requeridos... Verifique por favor");
+    } else {
+      const existeProducto = this.products.find((p) => p.code === code);
+      if (existeProducto) {
+        console.log(`El producto con código ${code} ya existe.`);
+      } else {
+        this.products.push(productObj);
+        console.log(`El producto con código ${code} ha sido agregado.`);
+      }
+    }
+  }
+  getProducts() {
+    // Debe contar con un método “getProducts” el cual debe devolver el arreglo con todos los productos creados hasta ese momento
+    return this.products;
+  }
+  getProductById(id) {
+    // Debe contar con un método “getProductById” el cual debe buscar en el arreglo el producto que coincida con el id
+    // En caso de no coincidir ningún id, mostrar en consola un error “Not found”
+    return this.products.find((p) => p.id === id) ?? "Product not found";
+  }
+}
+console.clear();
+console.log('********************************')
+// Se crea instancia prod1 de la clase productManager
+const prod1 = new productManager();
+
+prod1.addProduct("lee", "pantalon", 25, "cualquyie", "asd123", 15);
+prod1.addProduct("levis", "camisa", 40, "otro thum", "asd654", 15);
+prod1.addProduct("lee", "short", 15, "cual", "asd123", 5);
+prod1.addProduct("lee", "short", 15, "cual", "asd123");
+prod1.addProduct("cortefiel", "abrigo", 140, "otro thum", "rew952", 6);
+
+console.log(prod1.getProducts());
+console.log(prod1.getProductById(2));
