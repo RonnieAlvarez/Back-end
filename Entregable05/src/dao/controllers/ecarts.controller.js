5d854726e5860dbeb2d69f82584e4a8424669dfb
RonnieAlvarez <ronnie.alvarezc@gmail.com>
2023-04-15 15:53:46 -0600

    sabado 3.53

--- Entregable05/src/dao/controllers/ecarts.controller.js
+++ Entregable05/src/dao/controllers/ecarts.controller.js
@@ -5,27 +5,27 @@ import { CartModel } from "../models/ecommerce.model.js";
 
 export async function getRealCarts(req, res) {
   try {
-    let carts = await CartService.getAllCarts();
-    let aId=0
-    let apid=0
-    let aQua=0
-    let acarts=[]
-    carts.forEach((event) => {
-      aId=event.id
-      if (event.products.length>0){
-        const {
-          _doc: { products: [{ _doc: { pid, Quantity } }] },
-        } = event 
-        apid=pid ? pid:0
-        aQua=Quantity ? Quantity:0
-      }else{
-        apid=0
-        aQua=0
-      }
-        acarts.push({id:aId,pid:apid,Quantity:aQua})
-      }); 
-
-    return res.render("realTimeCarts", { cartsa: acarts });
+    // let carts = await CartService.getAllCarts();
+    // let aId=0
+    // let apid=0
+    // let aQua=0
+    // let acarts=[]
+    // carts.forEach((event) => {
+    //   aId=event.id
+    //   if (event.products.length>0){
+    //     const {
+    //       _doc: { products: [{ _doc: { pid, Quantity } }] },
+    //     } = event 
+    //     apid=pid ? pid:0
+    //     aQua=Quantity ? Quantity:0
+    //   }else{
+    //     apid=0
+    //     aQua=0
+    //   }
+    //     acarts.push({id:aId,pid:apid,Quantity:aQua})
+    //   }); 
+const carts = await CartModel.find()
+    return res.render("realTimeCarts", { carts: carts });
   } catch (error) {
     res.status(400).json({
       error: error.message,
@@ -105,9 +105,11 @@ export async function saveProductToCart(req,res) {
     let id = parseInt(body.id);
     let pid = parseInt(body.pid);
     let Quantity = parseInt(body.Quantity);
-    //const { pid, Quantity } = body;
-    //pid=parseInt(pid);
-    //Quantity=parseInt(Quantity);
+    
+/* This code block is checking if the `id` parameter is not provided in the request body. If `id` is
+not provided, it retrieves all the carts using `CartService.getAllCarts()` and finds the maximum
+`id` value among them. It then assigns `id` to be the maximum `id` value plus 1, which ensures that
+the new cart being created will have a unique `id`. */
 if (!id){
   const Carts = await CartService.getAllCarts();
   let maxId=0
@@ -116,6 +118,11 @@ if (!id){
     });  
    id = maxId + 1;
 }
+/* This code block is checking if a cart with the given `id` exists in the database. If it does not
+exist, a new cart is created with the provided `id`, `pid`, and `Quantity` values. If it does exist,
+the cart is updated with the provided `pid` and `Quantity` values. The `populate('products')` method
+is used to populate the `products` field of the cart with the actual product objects instead of just
+their ids. */
 let cart = await CartModel.findOne({id:id}).populate('products')
 if (!cart){
 let newCart = await CartModel.create({
@@ -123,16 +130,19 @@ let newCart = await CartModel.create({
   products: {pid: pid,Quantity: Quantity}
   });
 }else{
-  let newCart = await CartModel.updateOne({
-    id: id,
-    products: {pid: pid,Quantity: Quantity}
-    });
+  //let Data = cart.products
+  //agregar productos a un array y populate?
+  //cart.products.unshift({pid: pid,Quantity: Quantity}) 
+//   let newCart = await CartModel.updateOne({
+//     id: id,
+//     products: products.unshift({pid: pid,Quantity: Quantity}) 
+//     });
+const filter = { id: id };
+const update = { $push: { products: { pid: pid, Quantity: Quantity } } };
+const options = { new: true };
+let updatedCart = await CartModel.findOneAndUpdate(filter, update, options);
 }
 
-     
-    //CartModel.update
-    //carts.save
-  //  let carts = await CartModel.find({}).populate('products')
     let carts = await CartService.getAllCarts();
     let aId=0
     let apid=0
